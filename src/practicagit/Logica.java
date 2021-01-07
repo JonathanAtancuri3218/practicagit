43e7368ebe4d49b419745f57dc23e75fcff9ce8a
JonathanAtancuri3218 <JOTA5613jhon>
2021-01-07 13:49:22 -0500

    Proyecto Base

---
+++ src/practicagit/Logica.java
@@ -0,0 +1,11 @@
+package practicagit;
+
+public class Logica {
+	
+	public String metodoIntegrante1(){
+		return "integrante1";
+	}
+	public String metodoIntegrante2(){
+		return "integrante2";
+	}
+}
---
+++ src/practicagit/Principal.java
@@ -0,0 +1,7 @@
+package practicagit;
+
+public class Principal {
+	
+	
+
+}
