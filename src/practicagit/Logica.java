140e30778073930a2a3dfac77bf54da7c7ad2741
EstebanSibri <74636163+EstebanSibri@users.noreply.github.com>
2021-01-07 15:45:36 -0500

    Cmabios dos S

--- src/practicagit/Logica.java
+++ src/practicagit/Logica.java
@@ -3,7 +3,8 @@ package practicagit;
 public class Logica {
 	
 	public String metodoIntegrante1(){
-		return "Este Metodo a sido modificado por Jonathan";
+		return "Este Metodo a sido modificado por Jonathan Atancuri";
+
 	}
 	public String metodoIntegrante2(){
 		return "Este Metodo a sido modificado por Sibri";
