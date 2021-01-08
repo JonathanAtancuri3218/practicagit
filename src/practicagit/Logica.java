a29d22f206f8b7c360697e39fa42fa9a4119cdb6
JonathanAtancuri3218 <JOTA5613jhon>
2021-01-07 19:15:39 -0500

    atancuri agrego 2

--- src/practicagit/Logica.java
+++ src/practicagit/Logica.java
@@ -3,12 +3,9 @@ package practicagit;
 public class Logica {
 	
 	public String metodoIntegrante1(){
-<<<<<<< HEAD
 
-		return "Este Metodo a sido modificado por Jonathan 1";
-=======
-		return "Este Metodo a sido modificado por Jonathan Atancuri  holamundo";
->>>>>>> atancuri
+		return "Este Metodo a sido modificado por Jonathan Atancuri  1";
+
 	}
 	public String metodoIntegrante2(){
 		return "Este Metodo a sido modificado por Sibri";
